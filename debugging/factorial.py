7f8bd63fc7b495377f7563053ed724993e2ab985
roya86 <royaabduallah@gmail.com>
2025-11-23 16:02:57 +0300

    Task 0

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,9 +5,9 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-       n -= 1
+        n -= 1
     return result
 
-if __name__ == "__main__":
-    f = factorial(int(sys.argv[1]))
-    print(f)
+f = factorial(int(sys.argv[1]))
+print(f)
+
